e5200e829309ac0b453df35f57738ab3ee9de3a5
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2022-12-21 12:10:12 +0100

    Add preprocessing of Papyrus

--- src/mutants-in-pcm/preprocessing.py
+++ src/mutants-in-pcm/preprocessing.py
@@ -3,19 +3,15 @@
 
 """Data sourcing and pre-processing."""
 
-import os
-import re
-from typing import Optional, Union
-from functools import partial
-from itertools import chain
+from typing import Union, Iterator
 
-import pystow
-import chembl_downloader
+from tqdm.auto import tqdm
 import papyrus_scripts
+import chembl_downloader
 
 import pandas as pd
-from rdkit import Chem
 from pandas.io.parsers import TextFileReader as PandasTextFileReader
+from papyrus_scripts.utils import IO as papyrusIO
 
 
 def obtain_chembl_data(chembl_version: str, chunksize: int = None, data_folder: str = None):
@@ -38,7 +34,7 @@ def obtain_papyrus_data(papyrus_version: str, flavor: str, chunksize: int = None
     """Obtain mutant data from Papyrus using Papyrus-scripts.
 
     :param papyrus_version: version of chembl to work with
-    :param flavor: flavor of the Papyrus dataset to work with
+    :param flavor: flavor of the Papyrus dataset to work with; one of {nostereo_pp, nostereo, stereo}
     :param chunksize: size of chunks of data to be used (default: None)
     param data_folder: path to the folder in which the Papyrus
     dataset is located or will be downloaded (default:
@@ -46,12 +42,54 @@ def obtain_papyrus_data(papyrus_version: str, flavor: str, chunksize: int = None
     :return: a Pandas dataframe if chunksize is None, a pandas.io.parsers.TextFileReader otherwise
     """
     # 1) Verify version is available online
-    # 2) Verify version is available locally
-    # 3) If not download
-    # 4) Filter mutant data
-    return
+    online_versions = papyrusIO.get_online_versions()
+    if papyrus_version not in online_versions:
+        raise ValueError(f'Papyrus version must be one of [{", ".join(online_versions)}]')
+
+    # Verify flavor
+    flavor = flavor.lower()
+    if flavor not in ['nostereo_pp', 'nostereo', 'stereo']:
+        raise ValueError('Flavor of the Papyrus data must be one of {\'nostereo_pp\', \'nostereo\', \'stereo\'}')
+    # Define arguments for parsing and download
+    only_pp = flavor.endswith('pp')
+    stereo = flavor.startswith('stereo')
+
+    # 2) Ensure version is available locally
+    # This either downloads or verifies file hashes
+    papyrus_scripts.download_papyrus(data_folder, papyrus_version,
+                                     nostereo=(not stereo), stereo=stereo, only_pp=only_pp,
+                                     structures=False, descriptors='none', disk_margin=0)
+    # Make sure version is correct (transform 'latest' to a version number)
+    papyrus_version = papyrusIO.process_data_version(papyrus_version, data_folder)
+
+    # 3) Filter mutant data
+    papyrus_data = papyrus_scripts.read_papyrus(is3d=stereo, version=papyrus_version, plusplus=only_pp,
+                                                chunksize=chunksize, source_path=data_folder)
+
+    return _keep_papyrus_mutants(papyrus_data)
+
+
+def _keep_papyrus_mutants(data: Union[PandasTextFileReader, pd.DataFrame]) -> Union[Iterator, pd.DataFrame]:
+    """Keep Papyrus data related to mutant proteins only
+
+    :param data: Papyrus data (raw or preprocessed)
+    """
+    if isinstance(data, (PandasTextFileReader, Iterator)):
+        return _chunked_keep_papyrus_mutants(data)
+    else:
+        return data[data.Protein_Type != 'WT']
+
+
+def _chunked_keep_papyrus_mutants(data: Union[PandasTextFileReader, Iterator]) -> Iterator:
+    """Keep Papyrus data related to mutant proteins only
+
+    :param data: Papyrus data (raw or preprocessed)
+    """
+    for chunk in data:
+        yield _keep_papyrus_mutants(chunk)
 
 
 if __name__ == "__main__":
-    chembl_data = obtain_chembl_data('31', 100_000)
-    papyrus_data = obtain_papyrus_data('05.6', '++', 100_000)
+    # chembl_data = obtain_chembl_data('31', 100_000)
+    papyrus_data = obtain_papyrus_data('05.6', 'nostereo', 1_000_000)
+    print(sum(len(chunk.index) for chunk in tqdm(papyrus_data)))
