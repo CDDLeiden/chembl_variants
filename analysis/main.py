6392f290d2c3f8cd7d0397c01e053cc30d96b8c8
Marina Gorostiola González <m.gorostiola.gonzalez@lacdr.leidenuniv.nl>
2023-08-13 08:09:13 +0200

    Adapt main script to write modelling files for all accessions, not just a selection

--- analysis/main.py
+++ analysis/main.py
@@ -6,7 +6,7 @@ papyrus_version = '05.5'
 papyrus_flavor = 'nostereo'
 
 # Define annotation round
-annotation_round = 1
+annotation_round = 2
 
 # Define directories for this annotation round
 directories_file = './directories.json'
@@ -47,47 +47,34 @@ for accession in stats_protein['accession'].tolist():
     # Full dataset
     compute_variant_activity_distribution(annotated_data, accession, common=False, sim=False, sim_thres=None,
                                           threshold=None, variant_coverage=None, plot=True, hist=False, plot_mean=True,
-                                          color_palette=None, save_dataset=False,
+                                          color_palette=None, save_dataset=True,
                                           output_dir=common_analysis_dir)
     # Strict common subset with > 20% coverage
     compute_variant_activity_distribution(annotated_data, accession, common=True, sim=False, sim_thres=None,
                                           threshold=2, variant_coverage=0.2, plot=True, hist=False, plot_mean=True,
-                                          color_palette=None, save_dataset=False,
+                                          color_palette=None, save_dataset=True,
                                           output_dir=common_analysis_dir)
     # Common subset with > 20% coverage including similar compounds (>80% Tanimoto) tested in other variants
     compute_variant_activity_distribution(annotated_data, accession, common=True, sim=True, sim_thres=0.8,
                                           threshold=2, variant_coverage=0.2, plot=True, hist=False, plot_mean=True,
-                                          color_palette=None, save_dataset=False,
+                                          color_palette=None, save_dataset=True,
                                           output_dir=common_analysis_dir)
     # Strictly common subset (threshold=None)
     compute_variant_activity_distribution(annotated_data, accession, common=True, sim=False, sim_thres=None,
                                           threshold=None, variant_coverage=None, plot=True, hist=False, plot_mean=True,
-                                          color_palette=None, save_dataset=False,
-                                          output_dir=common_analysis_dir)
-print('Done.')
-# Write datasets for modelling for the relevant targets of interest (biggest common subsets)
-print('Extracting targets with big common subsets...')
-accession_large_common_subsets = extract_relevant_targets(common_analysis_dir,
-                         common=True, sim=True, sim_thres=0.8, threshold=2,variant_coverage=0.2,
-                         min_subset_n= 90, thres_error_mean=0, error_mean_limit='min')['accession'].unique().tolist()
-print('Done.')
-print('Writing datasets for modelling...')
-for accession in accession_large_common_subsets:
-    # Full dataset
-    compute_variant_activity_distribution(annotated_data, accession, common=False, sim=False, sim_thres=None,
-                                          threshold=None, variant_coverage=None, plot=False, hist=False, plot_mean=True,
-                                          color_palette=None,save_dataset=True,
-                                          output_dir=common_analysis_dir)
-    # Common subset with > 20% coverage including similar compounds (>80% Tanimoto) tested in other variants
-    compute_variant_activity_distribution(annotated_data, accession, common=True, sim=True, sim_thres=0.8,
-                                       threshold=2,variant_coverage=0.2, plot=False, hist=False, plot_mean=True,
-                                       color_palette=None,save_dataset=True,
+                                          color_palette=None, save_dataset=True,
                                           output_dir=common_analysis_dir)
 print('Done.')
 ########################################################################################################################
 # Step 4: Compute bioactivity distributions for compound clusters for proteins with the biggest common subsets
 from mutant_analysis_compounds import plot_bioactivity_distribution_cluster_subset
 from mutant_analysis_common_subsets import plot_bubble_bioactivity_distribution_stats
+# Extract relevant targets of interest (biggest common subsets)
+print('Extracting targets with big common subsets...')
+accession_large_common_subsets = extract_relevant_targets(common_analysis_dir,
+                         common=True, sim=True, sim_thres=0.8, threshold=2,variant_coverage=0.2,
+                         min_subset_n= 90, thres_error_mean=0, error_mean_limit='min')['accession'].unique().tolist()
+print('Done.')
 print('Computing bioactivity distributions for compound clusters...')
 for accession in accession_large_common_subsets:
     # Compute clusters and plot bioactivity distributions for each cluster
