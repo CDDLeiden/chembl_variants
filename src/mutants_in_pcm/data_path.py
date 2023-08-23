378c54790003722ee038999846d90d17e882a85a
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2023-08-23 13:57:27 +0200

    Make pip installable

--- src/mutants_in_pcm/data_path.py
+++ src/mutants_in_pcm/data_path.py
@@ -2,7 +2,7 @@ import os
 
 def get_data_path():
     data_dir = 'C:\\Users\\gorostiolam\\Documents\\Gorostiola Gonzalez, ' \
-           'Marina\\PROJECTS\\6_Mutants_PCM\\PROTOCOLS-SCRIPTS\\Python\\mutants-in-pcm\\data'
+           'Marina\\PROJECTS\\6_Mutants_PCM\\PROTOCOLS-SCRIPTS\\Python\\mutants_in_pcm\\data'
     data_dir = os.path.abspath(data_dir)
     if os.path.exists(data_dir):
         return data_dir
